9e8e000a112b692168e778fa6b9ce73fcc2fc455
rochams <msrocha.ad@gmail.com>
2022-04-26 17:39:11 -0300

    melhorias na views

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,13 @@
 from django import views
 from django.urls import path
-from .views import list_tarefas, gerenciar_tarefas
+#from .views import list_tarefas, gerenciar_tarefas
+from .views import TarefasView, Gerencia_Tarefas
 
 
 urlpatterns = [
-    path('', list_tarefas),
-    path('<int:pk>', gerenciar_tarefas)
+    #path('', list_tarefas),
+    #path('<int:pk>', gerenciar_tarefas),
+    path('', TarefasView.as_view()),
+    path('<int:pk>', Gerencia_Tarefas.as_view())
 ]
 
--- app/views.py
+++ app/views.py
@@ -3,9 +3,56 @@ from rest_framework.decorators import api_view
 from .serializers import Tarefa_serializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.exceptions import NotFound
 
 
-@api_view(['GET', 'POST'])
+
+class TarefasView(APIView):
+    def get(self, request):
+        tarefas = Tarefas.objects.all()
+        serializer = Tarefa_serializer(tarefas, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = Tarefa_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Gerencia_Tarefas(APIView):
+    def get_object(self, pk):
+        try:
+            return Tarefas.objects.get(pk=pk)
+        except:
+            raise NotFound()
+
+
+    def get(self, request, pk):
+        tarefas = self.get_object(pk)
+        serializer = Tarefa_serializer(tarefas)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        tarefas = self.get_object(pk)
+        serializer = Tarefa_serializer(tarefas, request.data)
+        if serializer.is_valid():   
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        tarefas = self.get_object(pk)
+        tarefas.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+        
+
+"""@api_view(['GET', 'POST'])
 def list_tarefas(request):
     if request.method == 'GET':
         tarefas = Tarefas.objects.all()
@@ -16,12 +63,12 @@ def list_tarefas(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)"""
 
 
 # O nome da função será o nome do título colocado na página de manipulação.
 
-@api_view(['GET', 'PUT', 'DELETE'])
+"""@api_view(['GET', 'PUT', 'DELETE'])
 def gerenciar_tarefas(request, pk):
     try:
         tarefas = Tarefas.objects.get(pk=pk)
@@ -39,4 +86,4 @@ def gerenciar_tarefas(request, pk):
         return Response(status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         tarefas.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)"""
